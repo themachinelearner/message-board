72477ea0683fefc6a4bf8d079ff93d3b0de655d7
Ben Smith <wiredcowboys@gmail.com>
2023-06-07 21:06:36 -0600

    Render messages on home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+let messages = [
+  {
+    text: "Hello There!",
+    user: "Amanda",
+    posted: new Date()
+  },
+  {
+    text: "Get oughta here!",
+    user: "Mikey",
+    posted: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages });
+});
+
+router.post('/new', function(req, res, next) {
+
 });
 
 module.exports = router;
